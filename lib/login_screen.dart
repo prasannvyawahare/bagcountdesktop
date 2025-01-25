1d19571ae12bba643f940529b1b0eb47d5ae0e4a
prasann <prasann.vyawahare@kratinsolutio>
2025-01-25 10:16:53 -0500

    kk

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,6 @@
+import 'package:bagreportun/SQLite/database_helper.dart';
+import 'package:bagreportun/master_screen.dart';
+import 'package:bagreportun/model/user.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,14 +15,44 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  late DatabaseHelper handler;
+  final String _errorMessage = '';
+  final db = DatabaseHelper();
 
-  String _errorMessage = '';
+ @override
+  void initState() {
+    handler = db;
+    super.initState();
+  }
 
   _login() async {
     String username = _usernameController.text;
     String password = _passwordController.text;
-    print(username);
-    print(password);
+    print("object");
+  //  print(username);
+   // print(password);
+      // Navigator.push(context, MaterialPageRoute(builder: (context) => MasterScreen()));
+
+    try {
+      if(await handler.checkUserCredentials(username, password)){
+        print("User already exists");
+         Navigator.push(context,  MaterialPageRoute( 
+                            builder: (context) => 
+                                MasterScreen()));
+      }else{
+        print("User does not exist");
+          var user = User(username: username, password: password);
+          //handler.insertUser(user);
+          Navigator.push(context,  MaterialPageRoute( 
+                            builder: (context) => 
+                                MasterScreen()));
+      }
+    
+    } catch (e) {
+      //print(e);
+      print("error");
+    }
+  
    // bool isValid = await _userRepository.validateLogin(username, password);
     // if (isValid) {
     //   // Proceed to the next screen or show success
@@ -64,10 +97,20 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
-            ),
+           ElevatedButton(
+                        onPressed: () {
+                       //   print("object");
+                       _login();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                        ),
+                        child: Text("Submit", style: TextStyle(color: Colors.white)),
+                      ),
           ],
         ),
       ),
